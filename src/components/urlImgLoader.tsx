00c56cc518a0de22f930e18b1c720b89ee78a38a
Aryansh <89254856+4127157@users.noreply.github.com>
2022-10-28 15:53:11 +0530

    todo

--- src/components/urlImgLoader.tsx
+++ src/components/urlImgLoader.tsx
@@ -1,4 +1,8 @@
 
+/* TODO: 
+ * Choose traditional fetch or axios
+ * GET image from URL as blob
+ * return image and set to state then send to preview comps */
 function urlImgLoader(){
     console.log("testing call");
 }
